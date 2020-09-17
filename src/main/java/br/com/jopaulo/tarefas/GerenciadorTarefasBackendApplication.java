1307d13e1467c4ef7ca41ed5ede52ccdf232d735
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-09-17 00:19:02 -0300

    Configurando o processo de validação para retornar os erros corretamente

--- src/main/java/br/com/jopaulo/tarefas/GerenciadorTarefasBackendApplication.java
+++ src/main/java/br/com/jopaulo/tarefas/GerenciadorTarefasBackendApplication.java
@@ -2,8 +2,12 @@ package br.com.jopaulo.tarefas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import br.com.jopaulo.tarefas.domain.task.Task;
 
@@ -19,4 +23,16 @@ public class GerenciadorTarefasBackendApplication implements RepositoryRestConfi
 		config.exposeIdsFor(Task.class);
 	}
 
+	@Bean
+	public Validator validator() {
+		return new LocalValidatorFactoryBean();
+	}
+	
+	
+	@Override
+	public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+		Validator validator = validator();
+		validatingListener.addValidator("beforeCreate", validator);
+		validatingListener.addValidator("beforeSave", validator);
+	}
 }
